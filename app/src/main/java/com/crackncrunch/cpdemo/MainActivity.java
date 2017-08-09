e5d089a3fe254a38d700fcdc0262536ea3c5bd76
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-08-10 00:03:58 +0400

    Initialize the database

--- app/src/main/java/com/crackncrunch/cpdemo/MainActivity.java
+++ app/src/main/java/com/crackncrunch/cpdemo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.crackncrunch.cpdemo;
 
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
